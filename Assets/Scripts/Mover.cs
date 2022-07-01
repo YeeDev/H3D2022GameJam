42d1a9298fc676df1624eb0acc1936f5a293683a
Yee <yee.gamedev@gmail.com>
2022-07-01 17:09:12 -0500

    Mover Script Created

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NTR.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+        [SerializeField][Range(-10, 10)] float moveSpeed = 0;
+
+        Vector2 moveDirection;
+        Rigidbody rb;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            moveDirection.x = Input.GetAxisRaw("Horizontal");
+            moveDirection.y = Input.GetAxisRaw("Vertical");
+        }
+
+        private void FixedUpdate()
+        {
+            Vector3 moveVelocity = new Vector3(moveDirection.x, 0, moveDirection.y).normalized * moveSpeed;
+            rb.MovePosition(transform.position + moveVelocity);
+        }
+    }
+}
