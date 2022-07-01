bed79c9c61812e648c38400661f7c8337bbca286
Yee <yee.gamedev@gmail.com>
2022-07-01 17:49:45 -0500

    Player Animator Controller Created

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -8,6 +8,7 @@ namespace NTR.Movement
     {
         [SerializeField][Range(-10, 10)] float moveSpeed = 0;
 
+        bool facingLeft;
         Vector2 moveDirection;
         Rigidbody rb;
 
@@ -20,12 +21,29 @@ namespace NTR.Movement
         {
             moveDirection.x = Input.GetAxisRaw("Horizontal");
             moveDirection.y = Input.GetAxisRaw("Vertical");
+
+            if (facingLeft && moveDirection.x > 0)
+            {
+                Flip();
+            }
+            else if (!facingLeft && moveDirection.x < 0)
+            {
+                Flip();
+            }
         }
 
         private void FixedUpdate()
         {
             Vector3 moveVelocity = new Vector3(moveDirection.x, 0, moveDirection.y).normalized * moveSpeed;
             rb.MovePosition(transform.position + moveVelocity);
+        }
+
+        private void Flip()
+        {
+            facingLeft = !facingLeft;
+            Vector3 currentScale = transform.localScale;
+            currentScale.x *= -1;
+            transform.localScale = currentScale;
         }
     }
 }
