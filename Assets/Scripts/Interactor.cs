b1f086b9847ec4014ed9bb5a6953ed3cfcf94da3
Yee <yee.gamedev@gmail.com>
2022-07-02 12:16:10 -0500

    Interactor Script Created

---
+++ Assets/Scripts/Interactor.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    [SerializeField] Transform checker = null;
+    [SerializeField] [Range(0.01f, 5f)] float checkerRadius = 1;
+    [SerializeField] LayerMask interactablesMask = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            if (Physics.CheckSphere(checker.position, checkerRadius, interactablesMask))
+            {
+                Debug.Log("Interacting");
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (checker == null) { return; }
+
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(checker.position, checkerRadius);
+    }
+}
