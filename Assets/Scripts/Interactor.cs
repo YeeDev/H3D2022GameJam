4c2566d69f3b2d38ab093f998c54e767d76ef637
Yee <yee.gamedev@gmail.com>
2022-07-02 17:17:16 -0500

    Interactor Refactor to Use Controller

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace NTR.Interactions
@@ -13,33 +11,37 @@ namespace NTR.Interactions
 
         Ingredients ingredientCarrying = Ingredients.NONE;
 
-        void Update()
+        public void ChecForInteractionType()
         {
-            if (Input.GetKeyDown(KeyCode.K))
+            RaycastHit hit;
+            Physics.SphereCast(transform.position, checkerRadius, checker.position, out hit, 1f, interactablesMask);
+
+            if (hit.transform != null)
             {
-                RaycastHit hit;
-                Physics.SphereCast(transform.position, checkerRadius, checker.position, out hit, 1f, interactablesMask);
+                IInteraction interaction = hit.transform.GetComponent<IInteraction>();
 
-                if (hit.transform != null)
-                {
-                    IInteraction interaction = hit.transform.GetComponent<IInteraction>();
-
-                    if (interaction == null)
-                    {
-                        Ingredient ingredient = hit.transform.GetComponent<Ingredient>();
-
-                        ingredientCarrying = ingredient.GetIngredient;
-                        carriedIngredient.sprite = ingredient.GetSprite;
-                    }
-                    else
-                    {
-                        interaction.Interact(ingredientCarrying);
-
-                        ingredientCarrying = Ingredients.NONE;
-                        carriedIngredient.sprite = null;
-                    }
-                }
+                if (interaction == null) { IngredientInteraction(hit.transform); }
+                else { CookInteraction(interaction); }
             }
+        }
+
+        private void IngredientInteraction(Transform hitTransform)
+        {
+            Ingredient ingredient = hitTransform.GetComponent<Ingredient>();
+            SetIngredient(ingredient);
+        }
+
+        //TODO set in animation script?
+        private void SetIngredient(Ingredient ingredient)
+        {
+            ingredientCarrying = ingredient != null ? ingredient.GetIngredient : Ingredients.NONE;
+            carriedIngredient.sprite = ingredient != null ?  ingredient.GetSprite : null;
+        }
+
+        private void CookInteraction(IInteraction objectToInteractWith)
+        {
+            objectToInteractWith.Interact(ingredientCarrying);
+            SetIngredient(null);
         }
 
         private void OnDrawGizmos()
