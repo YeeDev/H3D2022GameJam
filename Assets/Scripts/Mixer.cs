29d317f780d8126d04a753ed7aae40906e6a08cd
Yee <yee.gamedev@gmail.com>
2022-07-02 12:53:13 -0500

    Mixer Interaction Created

---
+++ Assets/Scripts/Mixer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NTR.Interactions
+{
+    public class Mixer : MonoBehaviour, IInteraction
+    {
+        public void Interact()
+        {
+            Debug.Log("You are Mixing Ingredients!");
+        }
+    }
+}
