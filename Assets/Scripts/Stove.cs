6e7f2e9919c12efb1132b87ccfa48e526961afbd
Yee <yee.gamedev@gmail.com>
2022-07-02 12:53:31 -0500

    Stove Interaction Created

---
+++ Assets/Scripts/Stove.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NTR.Interactions
+{
+    public class Stove : MonoBehaviour, IInteraction
+    {
+        public void Interact()
+        {
+            Debug.Log("You are cooking with a pan!");
+        }
+    }
+}
