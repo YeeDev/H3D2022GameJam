9f4c340dc9b6a9271fa909d2010f8cef99ef8286
Yee <yee.gamedev@gmail.com>
2022-07-02 12:53:22 -0500

    Oven Interaction Created

---
+++ Assets/Scripts/Oven.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NTR.Interactions
+{
+    public class Oven : MonoBehaviour, IInteraction
+    {
+        public void Interact()
+        {
+            Debug.Log("Cooking something in the oven!");
+        }
+    }
+}
