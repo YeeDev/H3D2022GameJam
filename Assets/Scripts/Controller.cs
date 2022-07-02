b639f1b0aa6df414b118dfdfd41e568ece939505
Yee <yee.gamedev@gmail.com>
2022-07-02 17:06:17 -0500

    Controller Scripts Created

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using NTR.Movement;
+
+namespace NTR.Controllers
+{
+    [RequireComponent(typeof(Mover))]
+    public class Controller : MonoBehaviour
+    {
+        bool holdH;
+        bool holdV;
+        bool horizontalPressedLast;
+        Vector3 moveDirection;
+
+        Mover mover;
+
+        private void Awake()
+        {
+            mover = GetComponent<Mover>();
+        }
+
+        private void Update()
+        {
+            CalculateInputDirection();
+            CalculateAnimationParameters();
+        }
+
+        private void FixedUpdate()
+        {
+            MoveCharacter();
+        }
+
+        private void CalculateInputDirection()
+        {
+            moveDirection.x = Input.GetAxisRaw("Horizontal");
+            moveDirection.z = Input.GetAxisRaw("Vertical");
+        }
+
+        private void CalculateAnimationParameters()
+        {
+            holdH = Input.GetButton("Horizontal");
+            holdV = Input.GetButton("Vertical");
+
+            if (holdH && !holdV || holdV && Input.GetButtonDown("Horizontal")) { horizontalPressedLast = true; }
+            if (!holdH && holdV || holdH && Input.GetButtonDown("Vertical")) { horizontalPressedLast = false; }
+        }
+
+        private void MoveCharacter()
+        {
+            mover.MoveInDirection(moveDirection, horizontalPressedLast);
+        }
+    }
+}
