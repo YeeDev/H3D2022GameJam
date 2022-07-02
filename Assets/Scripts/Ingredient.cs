e0290770920e570bec2f2cf38eee4d3bf0914890
Yee <yee.gamedev@gmail.com>
2022-07-02 13:14:45 -0500

    Ingredient Script created

---
+++ Assets/Scripts/Ingredient.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NTR.Interactions
+{
+    public class Ingredient : MonoBehaviour
+    {
+        [SerializeField] Ingredients ingredient;
+
+        Sprite ingredientSprite;
+
+        public Sprite GetSprite { get => ingredientSprite; }
+        public Ingredients GetIngredient { get => ingredient; }
+
+        private void Awake() { ingredientSprite = GetComponent<SpriteRenderer>().sprite; }
+    }
+}
