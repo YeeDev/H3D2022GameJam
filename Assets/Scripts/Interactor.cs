f8f80012020ad958197614179a0d20e8bd655aaf
Yee <yee.gamedev@gmail.com>
2022-07-02 12:53:40 -0500

    Interactor Script Created

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -2,28 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interactor : MonoBehaviour
+namespace NTR.Interactions
 {
-    [SerializeField] Transform checker = null;
-    [SerializeField] [Range(0.01f, 5f)] float checkerRadius = 1;
-    [SerializeField] LayerMask interactablesMask = 0;
-
-    void Update()
+    public class Interactor : MonoBehaviour
     {
-        if (Input.GetKeyDown(KeyCode.K))
+        [SerializeField] Transform checker = null;
+        [SerializeField] [Range(0.01f, 5f)] float checkerRadius = 1;
+        [SerializeField] LayerMask interactablesMask = 0;
+        [SerializeField] SpriteRenderer carriedIngredient = null;
+
+        void Update()
         {
-            if (Physics.CheckSphere(checker.position, checkerRadius, interactablesMask))
+            if (Input.GetKeyDown(KeyCode.K))
             {
-                Debug.Log("Interacting");
+                RaycastHit hit;
+                Physics.SphereCast(transform.position, checkerRadius, checker.position, out hit, 1f, interactablesMask);
+
+                if (hit.transform != null)
+                {
+                    IInteraction interaction = hit.transform.GetComponent<IInteraction>();
+
+                    if (interaction == null)
+                    {
+                        carriedIngredient.sprite = hit.transform.GetComponent<SpriteRenderer>().sprite;
+                    }
+                    else
+                    {
+                        interaction.Interact();
+                    }
+                }
             }
         }
-    }
 
-    private void OnDrawGizmos()
-    {
-        if (checker == null) { return; }
+        private void OnDrawGizmos()
+        {
+            if (checker == null) { return; }
 
-        Gizmos.color = Color.magenta;
-        Gizmos.DrawWireSphere(checker.position, checkerRadius);
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireSphere(checker.position, checkerRadius);
+        }
     }
 }
