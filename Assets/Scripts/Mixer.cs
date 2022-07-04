2847329d026d1fae145026538c9c57321fc345c7
Yee <yee.gamedev@gmail.com>
2022-07-04 17:45:48 -0500

    Created Ingredient Now Add Mix State

--- Assets/Scripts/Mixer.cs
+++ Assets/Scripts/Mixer.cs
@@ -6,14 +6,26 @@ namespace NTR.Interactions
 {
     public class Mixer : MonoBehaviour, IInteraction
     {
+        [SerializeField] GameObject dough = null;
+        [SerializeField] Transform ingredientHolder = null;
+
+        Ingredient ingredientCreated;
         List<IngredientsTypes> ingredientsInMixer = new List<IngredientsTypes>();
 
         public void Interact(IngredientsTypes ingredient)
         {
             if (ingredient != IngredientsTypes.NONE) { ingredientsInMixer.Add(ingredient); }
 
+            if (ingredientsInMixer.Contains(IngredientsTypes.Flour) && ingredient == IngredientsTypes.NONE)
+            {
+                if (ingredientCreated != null)
+                {
+                    ingredientCreated.MixMore();
+                    return;
+                }
 
-
+                ingredientCreated = Instantiate(dough, ingredientHolder.position, Quaternion.identity).GetComponent<Ingredient>();
+            }
         }
     }
 }
