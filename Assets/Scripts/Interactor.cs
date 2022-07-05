11fc9d29469f9b9db431b877ee23f48b07bc3857
Yee <yee.gamedev@gmail.com>
2022-07-05 12:38:58 -0500

    Interactor Can use Utensils

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -5,9 +5,27 @@ namespace NTR.Interactions
 {
     public class Interactor : MonoBehaviour
     {
-        public void UseUtensil()
+        [SerializeField] Transform rayDirectioneer = null;
+        [SerializeField] LayerMask utensilMask = 0;
+
+        Transform utensilToUse;
+
+        public void TryToUseUtensil()
         {
-            Debug.Log("Using an utensil");
+            utensilToUse = SearchForUtensil();
+            if (utensilToUse != null)
+            {
+                utensilToUse.GetComponent<IUtensil>().UseUtensil();
+            }
+        }
+
+        private Transform SearchForUtensil()
+        {
+            RaycastHit hit;
+            Vector3 direction = rayDirectioneer.position - transform.position;
+            Physics.Raycast(transform.position, direction, out hit, 0.75f, utensilMask);
+
+            return hit.transform;
         }
     }
 }
