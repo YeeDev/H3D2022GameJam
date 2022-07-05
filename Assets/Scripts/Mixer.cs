f02cc654de718e399618dc43863ad6307b8fb62b
Yee <yee.gamedev@gmail.com>
2022-07-05 12:38:38 -0500

    Mixer Script Created

---
+++ Assets/Scripts/Mixer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NTR.Utensils
+{
+    public class Mixer : MonoBehaviour, IUtensil
+    {
+        public void UseUtensil()
+        {
+            Debug.Log("You are using the mixer!");
+        }
+    }
+}
